16481c2a70311884fcdf3165b76490045f4afc5e
Davis Whitehead <whitehead.davis@gmail.com>
2023-01-17 18:52:52 -0500

    fixed crazy white bar

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,21 @@ export default class Document extends NativebaseDocument {
   static async getInitialProps(ctx) {
     const props = await super.getInitialProps(ctx);
     const styles = [
-      <style key={"fontsCSS"} dangerouslySetInnerHTML={{ __html: fontsCSS }} />,
+      <style
+        key={"fontsCSS"}
+        dangerouslySetInnerHTML={{ __html: fontsCSS }}
+      ></style>,
+      // below is required based on.... https://docs.nativebase.io/install-next#h3-run-the-hello-world-example and https://necolas.github.io/react-native-web/docs/setup/#root-element
+      <style key={"rootElementStyles"}>
+        {`
+          /* These styles make the body full-height */
+          html, body { height: 100%; }
+          /* These styles disable body scrolling if you are using <ScrollView> */
+          body { overflow: hidden; }
+          /* These styles make the root element full-height */
+          #root { display:flex; height:100%; }
+        `}
+      </style>,
       ...props.styles,
     ];
     return { ...props, styles: React.Children.toArray(styles) };
