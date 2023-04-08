7b3d5a9b9c3b401c49fc941509394d315315dc96
Davis Whitehead <whitehead.davis@gmail.com>
2023-04-08 18:10:20 -0400

    beginnings of the subscribe ui

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
-import { Button, FormControl, Input, Modal } from "native-base";
+import { FormControl, Input, Modal, Stack, Text } from "native-base";
 import { trackEvent, composeAction } from "../lib/gtag";
 import { categories, actions, objects } from "../lib/analyticsDefinitions";
+import Avatar from "./Avatar";
+// import H3 from "./typeography/H3";
+import { space } from "../styling/spacing";
+import LinkWithIcon from "./LinkWithIcon";
 
 interface Props {
   modalVisible: boolean;
@@ -24,27 +28,57 @@ export default function SubscribeModal(props: Props) {
       onClose={() => props.setModalVisible(false)}
       avoidKeyboard
       size="lg"
+      _backdrop={{
+        _dark: {
+          // bg: "orange.400",
+          bg: "black",
+          opacity: 0.8,
+        },
+        bg: "warmGray.50",
+      }}
     >
-      <Modal.Content position="sticky" marginTop={0} top="200" bottom="200">
+      <Modal.Content position="sticky" marginTop={0} top="375" bottom="375">
         <Modal.CloseButton />
-        <Modal.Header>Coming Soon</Modal.Header>
         <Modal.Body>
-          Coming soon!
-          <FormControl mt="3">
-            <FormControl.Label>Email</FormControl.Label>
-            <Input />
-          </FormControl>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            flex="1"
-            onPress={() => {
-              props.setModalVisible(false);
-            }}
+          <Stack
+            direction="column"
+            alignItems="center"
+            space={0}
+            py={space.lg}
+            px={space.lg}
           >
-            Close me
-          </Button>
-        </Modal.Footer>
+            <Avatar />
+            <Text
+              fontFamily="heading"
+              // fontWeight="200"
+              fontSize="md"
+              textAlign="center"
+            >
+              Subscribe to my newsletter
+            </Text>
+            <Text
+              fontFamily="body"
+              fontWeight="200"
+              fontSize="sm"
+              textAlign="center"
+            >
+              Reflections on what it means to live a well-lived life -- product
+              pitches, life experiments, and personal vignettes.
+            </Text>
+            <FormControl mt="3">
+              <FormControl.Label>Email</FormControl.Label>
+              <Input />
+            </FormControl>
+            <LinkWithIcon
+              onPress={() => {
+                props.setModalVisible(false);
+              }}
+              copy="No thanks"
+              target="_blank"
+              fontSize="sm"
+            />
+          </Stack>
+        </Modal.Body>
       </Modal.Content>
     </Modal>
   );
